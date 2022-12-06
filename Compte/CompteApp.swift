06558327f3e80329c936ff2b3a96f76a4f26adc7
Sergio Fresneda <sergio@sergiofresneda.com>
2022-12-06 10:28:13 +0100

    Added base project

---
+++ Compte/CompteApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompteApp.swift
+//  Compte
+//
+//  Created by likeadeveloper on 6/12/22.
+//
+
+import SwiftUI
+
+@main
+struct CompteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
