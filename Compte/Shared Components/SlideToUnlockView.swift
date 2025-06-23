a779eca117f4c395501f7638ee38e7c48f494a5b
sergio fresneda <fresneda@sergios-MacBook-Air.local>
2025-06-23 18:48:03 +0200

    Updated translations

--- Compte/Shared Components/SlideToUnlockView.swift
+++ Compte/Shared Components/SlideToUnlockView.swift
@@ -11,34 +11,29 @@ import SwiftUI
 struct SlideToUnlockView: View {
     // MARK: Vars
     @State private var sliderValue: Float = .zero
+    private let maxValue: Float = 100
     var action: (() -> Void)?
     
     // MARK: Lifecycle
     var body: some View {
         Slider(value: $sliderValue,
-               in: .zero...100,
+               in: .zero...maxValue,
                step: 5) {
             Text(NSLocalizedString("reset_counter",
                                    comment: "Reset Counter"))
         } onEditingChanged: { editing in
-            handleSliderChangeValue(isEditing: editing)
+            guard !editing else { return }
+            let value = sliderValue
+
+            withAnimation {
+                sliderValue = .zero
+            }
+
+            if value >= maxValue {
+                action?()
+            }
         }
         .tint(Color.suplementaryBackground)
         .foregroundColor(.red)
-    }
-}
-// MARK: - Helper
-private extension SlideToUnlockView {
-    func handleSliderChangeValue(isEditing: Bool) {
-        guard !isEditing else { return }
-        guard sliderValue == 100 else {
-            resetSlider()
-            return
-        }
-        action?()
-        resetSlider()
-    }
-    func resetSlider() {
-        sliderValue = .zero
     }
 }
