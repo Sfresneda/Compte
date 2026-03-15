d9e87efdccc0ccfb3f77222eb56e498556490d5f
sergio fresneda <sergio@sergiofresneda.com>
2026-03-15 13:21:44 +0100

    Removed non necesary files, added watch target

---
+++ Compte Watch Watch App/Compte_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Compte_WatchApp.swift
+//  Compte Watch Watch App
+//
+//  Created by sergio fresneda on 3/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Compte_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
